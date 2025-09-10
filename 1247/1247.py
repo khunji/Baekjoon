8fee4392be1ed2f7e413977aabf8a10d0efc355c
khunji <141021422+khunji@users.noreply.github.com>
2025-09-10 11:28:55 +0900

    Add files via upload

--- 1247/1247.py
+++ 1247/1247.py
@@ -1,1 +1,17 @@
+import sys
 
+input = sys.stdin.readline #빠른 입출력 속도 때문에 
+
+
+for i in range(3):  #3개의 테스트 케이스
+    N = int(input())
+    total=0
+    for j in range(N):
+        total+=int(input())
+
+    if total>0:
+        print("+")
+    elif total<0:
+        print('-')
+    else:
+        print("0")
