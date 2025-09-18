985d040b780facae7333010f4b19163bd2ad5d82
khunji <141021422+khunji@users.noreply.github.com>
2025-09-18 23:28:50 +0900

    Add files via upload

--- 1236/1236.py
+++ 1236/1236.py
@@ -1,1 +1,24 @@
+N,M = map(int,input().split())
 
+castle=[]
+
+for i in range(N):
+     castle.append(input())
+        
+countrow=0
+countcol=0
+
+for i in range(N):
+    if 'X' not in castle[i]:
+        countrow += 1
+
+for j in range(M):
+    found = False
+    for i in range(N):
+        if 'X' in castle[i][j]:
+            found=True
+            break
+    if not found:
+        countcol+=1
+
+print(max(countrow,countcol))
