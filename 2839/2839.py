3523b4884aa7f4f4a6f1c612774a7e0d24c448fa
khunji <141021422+khunji@users.noreply.github.com>
2025-09-23 00:04:17 +0900

    Add files via upload

--- 2839/2839.py
+++ 2839/2839.py
@@ -1,1 +1,18 @@
+N = int(input()) #설탕 N킬로그램
+#N은 3이상 5000이하
+result=0
+while N>=0:
+    if N % 5 == 0:
+        result+=N // 5
+        print(result)
+        break
+    N-=3
+    result+=1
+if N < 0:
+    print(-1)
+    
+
+
+
+
 
