e3b84299877a285f4dd5534fda2760bed177c390
khunji <141021422+khunji@users.noreply.github.com>
2025-09-28 18:07:05 +0900

    Add files via upload

--- 4153/4153.py
+++ 4153/4153.py
@@ -1,1 +1,10 @@
-
+while True:
+    a = list(map(int,input().split()))
+    if (a[0] and a[1] and a[2]) == 0:
+        break
+    max_a = max(a)
+    a.remove(max_a)
+    if a[0]**2+a[1]**2==max_a**2:
+        print('right')
+    else:
+        print('wrong')
