90357e0012875a16f4ba3ab2a905f7c7ec066f49
khunji <141021422+khunji@users.noreply.github.com>
2025-09-28 17:41:04 +0900

    Add files via upload

--- 10815/10815.py
+++ 10815/10815.py
@@ -1,1 +1,15 @@
+import sys #시간 초과 오류가 나서
+input = sys.stdin.readline
 
+N = int(input())
+sang_card = set(map(int,input().split()))
+
+M = int(input())
+card = list(map(int,input().split()))
+
+
+for i in card:
+    if i in sang_card:
+        print('1',end=' ')
+    else:
+        print('0',end=' ')
