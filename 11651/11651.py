698a3c826e3b01874a1e90d1d515a97600f52c0c
khunji <141021422+khunji@users.noreply.github.com>
2025-09-28 16:38:56 +0900

    Add files via upload

--- 11651/11651.py
+++ 11651/11651.py
@@ -1,1 +1,12 @@
+N = int(input()) #N개의 점
 
+number = [] #빈 리스트
+
+for i in range(N):
+   [x,y] = map(int,input().split())
+   number.append([y,x])
+
+number.sort()
+
+for i in number:
+   print(i[1],i[0])
