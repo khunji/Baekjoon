64230a42a8da32f7526a78a8252515e348ab88bd
khunji <141021422+khunji@users.noreply.github.com>
2025-09-28 16:38:13 +0900

    Add files via upload

--- 7568/7568.py
+++ 7568/7568.py
@@ -1,1 +1,14 @@
+N = int(input())
 
+number = []
+
+for i in range(N):
+    [w,h] = map(int,input().split())
+    number.append([w,h])
+
+for i in number:
+    rank=1
+    for j in number:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank+=1
+    print(rank, end=' ')
