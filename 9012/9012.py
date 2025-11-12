a88c9c2c3a4f8490a534b4096fb28fb95a76bc67
khunji <141021422+khunji@users.noreply.github.com>
2025-11-12 20:06:18 +0900

    Add files via upload

--- 9012/9012.py
+++ 9012/9012.py
@@ -1,1 +1,27 @@
+def solution(vps):
+    balance=0
 
+    for ch in vps:
+        if ch == '(':
+            balance+=1
+        else:
+            balance-=1
+
+        if balance < 0:
+            return "NO"
+    
+    if balance == 0:
+        return "YES"
+    else:
+        return "NO"
+        
+
+        
+
+N = int(input()) # 테스트 케이스
+
+
+for _ in range(N):
+    arr = input().strip()
+    print(solution(arr))
+    
