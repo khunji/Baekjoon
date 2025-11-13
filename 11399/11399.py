aa5bb1895076be82c5de1bc44f8f6f59fa78e16b
khunji <141021422+khunji@users.noreply.github.com>
2025-11-14 00:14:03 +0900

    Add files via upload

--- 11399/11399.py
+++ 11399/11399.py
@@ -1,1 +1,29 @@
+def bubble_sort(a):
+    n = len(a)
+    k=0
 
+    while k<n-1:
+        last = n-1
+        for j in range(n-1,k,-1):
+            if a[j-1] > a[j]:
+                a[j-1],a[j] = a[j],a[j-1]
+            last = j
+        k = last
+    
+    return a
+
+def minimum_sum(a):
+    
+    sum=0
+    n = len(a)
+    for i in range(n):
+        sum+=a[i]*(n-i)
+    return sum
+
+
+
+N = int(input())
+arr = list(map(int,input().split()))
+
+result = bubble_sort(arr)
+print(minimum_sum(result))
