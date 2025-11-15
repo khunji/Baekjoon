d4a0db38cc0365280a9d91fb9ddbc575dc5e3d17
khunji <141021422+khunji@users.noreply.github.com>
2025-11-15 23:02:33 +0900

    Add files via upload

--- 1920/1920.py
+++ 1920/1920.py
@@ -1,1 +1,18 @@
+import sys
 
+
+def solution(a1, a2):
+
+    s = set(a1)
+    result = []
+    for i in a2:
+        result.append("1" if i in s else "0")
+    print("\n".join(result))
+        
+
+input = sys.stdin.readline
+N = int(input())
+arr1 = list(map(int, input().split()))
+M = int(input())
+arr2 = list(map(int,input().split()))
+solution(arr1, arr2)
