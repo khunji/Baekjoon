8f98858066b0a4f6b5aedea4e65265143951668a
khunji <141021422+khunji@users.noreply.github.com>
2025-11-20 10:06:25 +0900

    Add files via upload

--- 1026/1026.py
+++ 1026/1026.py
@@ -1,1 +1,16 @@
+def solution(n,a,b):
+    newA = sorted(A) #오름차순
+    newB = sorted(B,reverse=True)#내림차순
+    S=0
 
+    for i in range(n):
+        S+=newA[i]*newB[i]
+    return S
+
+N = int(input()) #N
+A = list(map(int,input().split())) #A배열
+B = list(map(int,input().split())) #B배열
+
+result = solution(N,A,B)
+
+print(result)
