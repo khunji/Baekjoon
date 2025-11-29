af55d0b26c8821882bdea40f19136b1795d08b2b
khunji <141021422+khunji@users.noreply.github.com>
2025-11-29 14:04:52 +0900

    Add files via upload

--- 1735/1735.py
+++ 1735/1735.py
@@ -1,1 +1,15 @@
+import math
+
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+C=[None,None]
+
+
+C[0] = (A[0]*B[1]+B[0]*A[1])
+C[1] = (A[1]*B[1])
+
+
+print(int(C[0]/math.gcd(C[0],C[1])),int(C[1]/math.gcd(C[0],C[1])))
+
+
 
