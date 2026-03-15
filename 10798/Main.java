3be43473af92da6a6a80ae1d1218e910a6848000
khunji <141021422+khunji@users.noreply.github.com>
2026-03-15 19:59:55 +0900

    Add files via upload

--- 10798/Main.java
+++ 10798/Main.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in);
+        char[][] arr = new char[5][15];
 
+        for(int i=0; i<5; i++){
+            String word = scanner.next();
+            for(int j=0; j<word.length();j++){
+                arr[i][j] = word.charAt(j);
+            }
+        }
+
+        for(int j=0; j<15; j++){
+            for(int i=0; i<5; i++){
+                if(arr[i][j]!='\u0000'){
+                    System.out.print(arr[i][j]);
+                }
+            }
+        }
+
+
+        scanner.close();
+    }
+}
